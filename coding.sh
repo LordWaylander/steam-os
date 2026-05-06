35914ea09cc1cc3971fb02e7faf8d86f631a6f6a
LordWaylander <francois.caizes@laposte.net>
2026-05-06 20:23:23 +0200

    mise en place pour qemu
    Signed-off-by: LordWaylander <francois.caizes@laposte.net>

--- coding.sh
+++ coding.sh
@@ -17,8 +17,26 @@ pacman-key --populate "$CODENAME"
 
 echo "DESACTIVATION system read only"
 steamos-readonly disable
-echo "installation base-devel glibc linux-api-headers"
-pacman --sync --noconfirm --needed base-devel glibc linux-api-headers
+#echo "installation base-devel glibc linux-api-headers"
+#pacman --sync --noconfirm --needed base-devel glibc linux-api-headers
+echo "installation libvirt"
+pacman --sync --noconfirm --needed libvirt qemu-base dnsmasq
+
+echo "add user to libvirt group"
+usermod -aG libvirt deck
+
+echo "start libvirt service"
+systemctl start libvirtd.service #enable ?
+systemctl start dnsmasq
+
+firewall-cmd --permanent --new-zone=libvirt 2>/dev/null || true
+firewall-cmd --reload
+virsh net-start default 2>/dev/null || true
+virsh net-autostart default
+
+grep -qxF 'user = "deck"' /etc/libvirt/qemu.conf || echo 'user = "deck"' >> /etc/libvirt/qemu.conf
+grep -qxF 'group = "deck"' /etc/libvirt/qemu.conf || echo 'group = "deck"' >> /etc/libvirt/qemu.conf
+
 echo "ACTIVATION system read only"
 steamos-readonly enable
 
